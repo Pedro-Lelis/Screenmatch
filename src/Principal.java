d6c71f2973d89df5fe456ce3b7cdf2becacaba20
Pedro-Lelis <pedrinho.felipe2016@gmail.com>
2023-04-22 18:15:45 -0300

    Criando projeto screenmatch e iniciando a orientação a objetos

---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+import br.com.pedrolelis.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDelancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.getTotalDeAvaliacao());
+
+        System.out.println("Media: " + meuFilme.pegaMedia());
+    }
+}
